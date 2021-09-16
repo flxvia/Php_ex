d570e84db22f67da7a3e478e5ebd51a6f749b486
flxvia <flavia.mariasilvas25@gmail.com>
2021-09-16 20:56:32 -0300

    segundo ex

---
+++ ex2/exercicio_2.php
@@ -0,0 +1,34 @@
+<?php 
+
+	$horas = 6;
+	$salhora;
+	$numdep;
+	$inss;
+	$ir;
+
+	$salbruto = $horas *  $salhora + (50*$numdep);
+
+	if (salbruto <= 1000) {
+		$inss = $salbruto * (8.5 / 100);
+		echo "O desconto INSS do salário é de " . $inss . " reais.";
+	}
+	elseif (salbruto > 1000) {
+		$inss = $salbruto * (9 / 100);
+		echo "O desconto INSS do salário é de " . $inss . " reais.";
+		$ir = $salbruto * (7 / 100);
+		echo "O desconto IR do salário é de " . $ir . " reais.";
+	}
+
+	if (salbruto <= 500) {
+		$ir = 0;
+		echo "O desconto IR do salário é de " . $ir . " reais.";
+	}
+	elseif (salbruto <= 1000) {
+		$ir = $salbruto * (5 / 100);
+		echo "O desconto IR do salário é de " . $ir . " reais.";
+	}
+
+	$salliq = $salbruto - $inss - $ir;
+	echo "O salário líquido do colaborador é de " . $salliq . " reais";
+
+ ?>

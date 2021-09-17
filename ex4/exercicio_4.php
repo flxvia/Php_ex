2b25140cd443bc4eedf38b5b96e71a79864872fe
flxvia <flavia.mariasilvas25@gmail.com>
2021-09-16 21:37:57 -0300

    quarto ex

---
+++ ex4/exercicio_4.php
@@ -0,0 +1,9 @@
+<?php 
+
+$invalor = 1500;
+$finvalor = $invalor - $invalor * (9/100);
+
+echo "Estamos com um desconto de 9% em nossos produtos!";
+
+echo "<br/> Bicicleta de " . $invalor . " reais, agora por " . $finvalor . " reais.";
+ ?>

2eecc0b0352efcdfc6acb21d55c099789b5127e4
flxvia <flavia.mariasilvas25@gmail.com>
2021-09-16 21:07:05 -0300

    seg exercicio

--- ex2/exercicio_2.php
+++ ex2/exercicio_2.php
@@ -1,29 +1,29 @@
 <?php 
 
-	$horas = 6;
-	$salhora;
-	$numdep;
+	$horas = 220;
+	$salhora = 9;
+	$numdep = 3;
 	$inss;
 	$ir;
 
 	$salbruto = $horas *  $salhora + (50*$numdep);
 
-	if (salbruto <= 1000) {
+	if ($salbruto <= 1000) {
 		$inss = $salbruto * (8.5 / 100);
 		echo "O desconto INSS do salário é de " . $inss . " reais.";
 	}
-	elseif (salbruto > 1000) {
+	elseif ($salbruto > 1000) {
 		$inss = $salbruto * (9 / 100);
 		echo "O desconto INSS do salário é de " . $inss . " reais.";
 		$ir = $salbruto * (7 / 100);
 		echo "O desconto IR do salário é de " . $ir . " reais.";
 	}
 
-	if (salbruto <= 500) {
+	if ($salbruto <= 500) {
 		$ir = 0;
 		echo "O desconto IR do salário é de " . $ir . " reais.";
 	}
-	elseif (salbruto <= 1000) {
+	elseif ($salbruto <= 1000) {
 		$ir = $salbruto * (5 / 100);
 		echo "O desconto IR do salário é de " . $ir . " reais.";
 	}
